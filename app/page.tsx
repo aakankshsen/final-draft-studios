91879b6c817b9b0e8d7e6b368252db3d91bb82a5
aakankshsen <aakankshsen@gmail.com>
2026-09-25 11:43:18 +0530

    Removed how we work section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import ArrowTransition from "@/components/ArrowTransition";
 import Deliverables from "@/components/Deliverables";
 import Vision from "@/components/Vision";
-import Principles from "@/components/Principles";
+
 import Studio from "@/components/Studio";
 import ClientReel from "@/components/ClientReel";
 import Contact from "@/components/Contact";
@@ -19,7 +19,6 @@ export default function Home() {
       <ArrowTransition />
       <Deliverables />
       <Vision />
-      <Principles />
       <Studio />
       <Contact />
     </main>
